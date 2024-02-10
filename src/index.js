e1f9977699409dc8ce43c3d4e6edd0a1da93a86c
Vishnu Kumar <vermaji1891@gmail.com>
2024-02-10 10:55:55 +0530

    basic post request working fine

--- src/index.js
+++ src/index.js
@@ -15,6 +15,19 @@ app.listen({port:3000}, () => {
     console.log("Server started at port no 3000")
 })
 
+app.route({
+    url: "/home",
+    method: "GET",
+    handler: (req, res) => {
+        return {message:"Home Page Route"}
+    }
+})
+
+app.post("/home/:id", (req, res) => {
+    console.log(req.query, req.params, req.headers, req.body)
+    return {data: ["I am vishnu"]}
+})
+
 // try{
 //     await app.listen({port: 3000})
 //     console.log("Server Started")
