11dd8e41cbb167fcdd48205f60b4d4b468f4026d
Vishnu Kumar <vermaji1891@gmail.com>
2024-02-11 14:35:19 +0530

    added plugin and prefix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// import fastify from "fastify";
 const fastify = require("fastify")
 
 const serverConfig = require("./config/server_config")
@@ -7,36 +6,34 @@ const app = fastify({
     logger: true,
 })
 
+function myPlugin1 (fastify, options, done) {
+    console.log("this is plugin 1")
+    console.log(options)
+    fastify.get("/home", (req, res) => {
+        return {mess: "plugin return"}
+    })
+    done()
+}
+
+
+function myPlugin2 (fastify, options, done) {
+    console.log("this is plugin 2")
+    console.log(options)
+    done()
+}
+
+
+app.register(myPlugin1, {name: "vishnu", prefix: "/api"})
+app.register(myPlugin2, {name: "neha"})
+
 
 app.get("/ping", async (req, res) => {
     return {mess: "Ping Received Ok"}
 })
 
-app.get("/vishnu", async (req, res) => {
-    return {mess: "Vishnu is the lord of the universe"}
-})
 
-app.route({
-    url: "/home",
-    method: "GET",
-    handler: (req, res) => {
-        return {message:"Home Page Route"}
-    }
-})
-
-app.post("/home/:id", (req, res) => {
-    console.log(req.query, req.params, req.headers, req.body)
-    return {data: ["I am vishnu"]}
-})
 
 app.listen({port:3000}, () => {
     console.log(`Server started at port no ${serverConfig.PORT}`)
 })
 
-// try{
-//     await app.listen({port: 3000})
-//     console.log("Server Started")
-// }
-// catch(error){
-//     console.log(error)
-// }
